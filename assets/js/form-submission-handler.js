bab1b2a88b607dc59d5e4f55adc448bc5cf8e5b7
Martin De La Torre <funnygeeks101@gmail.com>
2026-04-05 17:46:16 -0500

    hopefully makes spinner line up with button on mobile

--- assets/js/form-submission-handler.js
+++ assets/js/form-submission-handler.js
@@ -65,7 +65,7 @@
       //First let's get the spinner going so they know we're doing stuff.
       var submitSpinner = form.querySelector("#submit_spinner");
       if (submitSpinner) {
-          submitSpinner.style.display = "block";
+          submitSpinner.style.display = "inline-block";
       }
 
       var submitButton = form.querySelector("#submit_button");
